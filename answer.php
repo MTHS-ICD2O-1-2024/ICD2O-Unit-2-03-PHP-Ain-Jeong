2233392914fcaf5b00d315aa828d9dafa27d46ef
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-03-19 16:35:52 +0000

    images

--- answer.php
+++ answer.php
@@ -1,45 +1,48 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="description" content="The address program,in PHP" />
-    <meta name="keywords" content="mths, icd2o" />
-    <meta name="author" content="Ain Jeong" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./css/style.css" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-light_blue.min.css" />
-    <link rel="apple-touch-icon" sizes="180x180" href="./images/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon-16x16.png" />
-    <link rel="manifest" href="./images/site.webmanifest" />
-    <title>The address program,in PHP</title>
-  </head>
-  <body>
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-      <header class="mdl-layout__header">
-        <div class="mdl-layout__header-row">
-          <span class="mdl-layout-title">The address program,in PHP</span>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="The address program,in PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Ain Jeong" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-light_blue.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
+  <link rel="manifest" href="./site.webmanifest" />
+  <title>The address program,in PHP</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">The address program,in PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/street.jpg" alt="street image" width="250" />
+      </div>
+      <div class="page-content-php">
+        <div id="address">
+          <?php
+          $streetName = $_GET["street-name"];
+          $streetNumber = $_GET["street-number"];
+          echo "<p>Your address is: " . $streetName . ", " . $streetNumber . ".</p>";
+          ?>
         </div>
-      </header>
-      <main class="mdl-layout__content">
-        <div class="right-image">
-          <img src="./images/street.jpg" alt="street image" width="250" />
-        </div>
-        <div class="page-content-php">
-          <div id="address">
-            <?php
-            $streetName = $_GET["street-name"];
-            $streetNumber = $_GET["street-number"];
-            echo "<p>Your address is: " . $streetName . ", " . $streetNumber . ".</p>";
-            ?>
-          </div>
-        </div>
-          <div class="page-content-return">
-            <a href="./index.php">Return ...</a>
-          </div>
-      </main>
-    </div>
-  </body>
+      </div>
+      <div class="page-content-return">
+        <a href="./index.php">Return ...</a>
+      </div>
+    </main>
+  </div>
+</body>
+
 </html>
--- index.php
+++ index.php
@@ -1,56 +1,59 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="description" content="The address program,in PHP" />
-    <meta name="keywords" content="mths, icd2o" />
-    <meta name="author" content="Ain Jeong" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./css/style.css" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-light_blue.min.css" />
-    <link rel="apple-touch-icon" sizes="180x180" href="./images/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon-16x16.png" />
-    <link rel="manifest" href="./images/site.webmanifest" />
-    <title>The address program,in PHP</title>
-  </head>
-  <body>
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-      <header class="mdl-layout__header">
-        <div class="mdl-layout__header-row">
-          <span class="mdl-layout-title">The address program,in PHP</span>
-        </div>
-      </header>
-      <main class="mdl-layout__content">
-        <div class="right-image">
-          <img src="./images/street.jpg" alt="street image" width="250" />
-        </div>
-        <div class="page-content-php">Please enter the following: </div>
-        <div class="page-content-answer">
-          <form action="answer.php" method="GET">
-            <p>Street Number</p>
-            <div class="mdl-textfield mdl-js-textfield">
-              <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" name="street-number">
-              <label class="mdl-textfield__label" for="street-number">Street number here ...</label>
-              <span class="mdl-textfield__error">Input is not a number!</span>
-            </div>
-            <br />
-            <p>Street Name</p>
-            <div class="mdl-textfield mdl-js-textfield">
-              <input class="mdl-textfield__input" type="text" name="street-name">
-              <label class="mdl-textfield__label" for="street-name">Street name here ...</label>
-            </div>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="The address program,in PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Ain Jeong" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-light_blue.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
+  <link rel="manifest" href="./site.webmanifest" />
+  <title>The address program,in PHP</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">The address program,in PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/street.jpg" alt="street image" width="250" />
+      </div>
+      <div class="page-content-php">Please enter the following: </div>
+      <div class="page-content-answer">
+        <form action="answer.php" method="GET">
+          <p>Street Number</p>
+          <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" name="street-number">
+            <label class="mdl-textfield__label" for="street-number">Street number here ...</label>
+            <span class="mdl-textfield__error">Input is not a number!</span>
+          </div>
           <br />
-        </div>
-          <!-- Accent-colored raised button with ripple -->
-            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-              type="submit">
-              Enter
-            </button>
-          </form>
-      </main>
-    </div>
-  </body>
+          <p>Street Name</p>
+          <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" name="street-name">
+            <label class="mdl-textfield__label" for="street-name">Street name here ...</label>
+          </div>
+          <br />
+      </div>
+      <!-- Accent-colored raised button with ripple -->
+      <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+        type="submit">
+        Enter
+      </button>
+      </form>
+    </main>
+  </div>
+</body>
+
 </html>
